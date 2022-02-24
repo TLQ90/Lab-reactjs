40563aba261fb8d3a995df5bc24675c65431df1b
tran le quyen <tlquyen1990@gmail.com>
2022-02-24 07:55:41 +0700

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+import Contact from './ContactComponent';
 import Header from './HeaderComponent';
 import Home from './HomeComponent';
 import Footer from './FooterComponent';
@@ -11,9 +15,12 @@ class Main extends Component {
 
   constructor(props) {
     super(props);
+
     this.state = {
-        dishes: DISHES,
-        selectedDish: null
+      dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     };
   }
 
@@ -25,6 +32,9 @@ class Main extends Component {
     const HomePage = () => {
       return(
           <Home 
+              dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
           />
       );
     }
@@ -33,6 +43,7 @@ class Main extends Component {
         <Header />
         <Switch>
               <Route path='/home' component={HomePage} />
+              <Route exact path='/contactus' component={Contact}/>
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
               <Redirect to="/home" />
         </Switch>
